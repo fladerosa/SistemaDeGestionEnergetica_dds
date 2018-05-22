b92817e72cf4091aabef4b40b7d580870503a220
Mauricio <Mauricio@ROCHA>
2018-05-22 18:14:44 -0300

    se agrego la clase Regla.

---
+++ SGE.Entidades/Reglas/Regla.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGE.Entidades.Reglas
+{
+    public class Regla
+    {
+        public string Nombre { get; set; }
+        public Sensor Sensor { get; set; }
+        public decimal ValorEsperado { get; set; }
+
+
+        public void Ejecutar()
+        {
+        }
+
+        public void TieneValorEsperado(Medicion xMedicion)
+        {
+        }
+    }
+}
