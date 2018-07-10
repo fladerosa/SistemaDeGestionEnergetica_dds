9f03914c70992adfeed9bc4e238ed5f278ac356f
Mauricio <mdrocha@est.frba.utn.edu.ar>
2018-07-10 11:11:56 -0300

    se agrego Zona y Transformador

---
+++ SGE.Entidades/Transformador.cs
@@ -0,0 +1,33 @@
+﻿using SGE.Entidades.Categorias;
+using SGE.Entidades.Dispositivos;
+using System.Collections.Generic;
+using System.Linq;
+using SGE.Entidades.Usuarios;
+
+namespace SGE.Entidades
+{
+    public class Transformador
+    {
+        #region Propiedades
+        public int Id { get; set; }
+        public decimal Latitud { get; set; }
+        public decimal Longitud { get; set; }
+        public List<Cliente> Clientes { get; set; }
+
+        public Transformador()
+        {
+            this.Clientes = new List<Cliente>();
+        }
+
+        public decimal ObtenerConsumo()
+        {
+            decimal consumo = 0;
+            foreach (Cliente cliente in Clientes)
+            {
+                consumo = consumo;
+            }
+            return consumo;
+        }
+        #endregion
+    }
+}
---
+++ SGE.Entidades/Zona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SGE.Entidades.Categorias;
+using SGE.Entidades.Dispositivos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+
+namespace SGE.Entidades
+{
+    public class Zona
+    {
+        #region Propiedades
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Latitud { get; set; }
+        public decimal Longitud { get; set; }
+        public decimal Radio { get; set; }
+        public List<Transformador> Transformadores { get; set; }
+
+
+        public Zona()
+        {
+            this.Transformadores = new List<Transformador>();
+        }
+
+        public decimal ObtenerConsumo()
+        {
+            decimal consumo = 0;
+            foreach (Transformador transformador in Transformadores)
+            {
+                consumo = consumo + transformador.ObtenerConsumo();
+            }
+            return consumo;
+        }
+
+        #endregion
+    }
+}
