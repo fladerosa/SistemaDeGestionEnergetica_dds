af29f148e43b8bd20eea7d8531109ebc3a1b4fa9
rbnkenny <rbn_125@hotmail.com>
2018-10-27 15:38:20 -0300

    Agregado de areas, menú y controllers

---
+++ SGE.WebconAutenticacion/Areas/Admin/Controllers/ReportesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGE.WebconAutenticacion.Areas.Admin.Controllers
+{
+    public class ReportesController : Controller
+    {
+        // GET: Admin/Reportes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SGE.WebconAutenticacion/Areas/Cliente/Controllers/HogarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGE.WebconAutenticacion.Areas.Cliente.Controllers
+{
+    public class HogarController : Controller
+    {
+        // GET: Cliente/Hogar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
