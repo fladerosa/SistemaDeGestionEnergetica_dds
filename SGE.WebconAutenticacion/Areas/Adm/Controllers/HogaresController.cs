419180774c7ee9c05f59ff5616ef123de826e25d
rbnkenny <rbn_125@hotmail.com>
2018-10-30 17:37:12 -0300

    Agregado de pantalla de hogares para el admin

--- SGE.WebconAutenticacion/Areas/Adm/Controllers/HogaresController.cs
+++ SGE.WebconAutenticacion/Areas/Adm/Controllers/HogaresController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SGE.Entidades.Reportes;
+using SGE.Entidades.Repositorio;
+using SGE.Entidades.Usuarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +14,14 @@ namespace SGE.WebconAutenticacion.Areas.Adm.Controllers
         // GET: Admin/Hogares
         public ActionResult Index()
         {
+            BaseRepositorio<Cliente> repoCliente = new BaseRepositorio<Cliente>();
+            ICollection<Cliente>clientes = repoCliente.GetAll();
+
+            ViewBag.clientes = clientes.Select(c => new SelectListItem() {
+                Text = c.NombreUsuario,
+                Value = Reporte.consumoPorHogarYPeriodo(c.Id, DateTime.Now.AddYears(-10), DateTime.Now).ToString(),
+            });
+
             return View();
         }
     }
